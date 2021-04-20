080056ac8c6aa44a7f4b87fbbb7cfb50d629b864
PolishHunter <o.szeferpldk@gmail.com>
2021-04-20 18:33:20 +0200

    Sign up view initial stage.

--- app/src/main/java/com/example/sep4_android/MainActivity.java
+++ app/src/main/java/com/example/sep4_android/MainActivity.java
@@ -3,8 +3,16 @@ package com.example.sep4_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button button;
+    RadioButton radioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
