066d052267afc08a1b7d7ccf87704af8b447f149
PolishHunter <o.szeferpldk@gmail.com>
2021-04-21 17:01:28 +0200

    Sign up view and Main app view update 2

--- app/src/main/java/com/example/sep4_android/MainAppActivity.java
+++ app/src/main/java/com/example/sep4_android/MainAppActivity.java
@@ -10,19 +10,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainAppActivity extends AppCompatActivity {
 
-    Button buttonAddGarden;
-    Button buttonViewGarden;
-    Button buttonSettings;
-
-    View view;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_main);
-
-        buttonAddGarden = findViewById(R.id.button_main_add_garden);
-        buttonViewGarden = findViewById(R.id.button_main_view_garden);
-        buttonSettings = findViewById(R.id.button_main_settings);
     }
 }
--- app/src/main/java/com/example/sep4_android/views/mainapp/MainPageFragment.java
+++ app/src/main/java/com/example/sep4_android/views/mainapp/MainPageFragment.java
@@ -7,14 +7,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.sep4_android.R;
 
 public class MainPageFragment extends Fragment {
 
+    Button buttonAddGarden;
+    Button buttonViewGarden;
+    Button buttonSettings;
+
+    View view;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main_page, container, false);
+        view = inflater.inflate(R.layout.fragment_main_page, container, false);
+        prepareUI();
+        return view;
     }
+
+    private void prepareUI() {
+
+        buttonAddGarden = view.findViewById(R.id.button_main_add_garden);
+        buttonViewGarden = view.findViewById(R.id.button_main_view_garden);
+        buttonSettings = view.findViewById(R.id.button_main_settings);
+    }
+
 }
