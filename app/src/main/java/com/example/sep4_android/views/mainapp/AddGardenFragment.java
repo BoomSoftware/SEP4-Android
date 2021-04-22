b7071cd5d7fb0d5ee9a02dd11c9f82e17c8647c0
Estera <dumi_estera@yahoo.com>
2021-04-22 14:33:36 +0200

    edited some text fields

--- app/src/main/java/com/example/sep4_android/views/mainapp/AddGardenFragment.java
+++ app/src/main/java/com/example/sep4_android/views/mainapp/AddGardenFragment.java
@@ -8,6 +8,8 @@ import androidx.lifecycle.ViewModelProvider;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.sep4_android.R;
 import com.example.sep4_android.viewmodels.AddNewGardenViewModel;
@@ -15,6 +17,12 @@ import com.example.sep4_android.viewmodels.AddNewGardenViewModel;
 public class AddGardenFragment extends Fragment {
     private AddNewGardenViewModel viewModel;
     private View view;
+    private EditText name;
+    private EditText landArea;
+    private EditText city;
+    private EditText street;
+    private EditText streetNumber;
+    private Button addGardenBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,7 +37,11 @@ public class AddGardenFragment extends Fragment {
 
 
     private void prepareUI(){
-
+        name = view.findViewById(R.id.input_garden_name);
+        landArea = view.findViewById(R.id.input_land_area);
+        city = view.findViewById(R.id.input_city);
+        street = view.findViewById(R.id.input_street);
+        streetNumber = view.findViewById(R.id.input_street_number);
     }
 
     private void prepareOnClickEvents(){
