d956129d701c8310f9fc7e71dea7517956d5ae9e
Estera <dumi_estera@yahoo.com>
2021-04-22 11:24:56 +0200

    add garden part 1

--- app/src/main/java/com/example/sep4_android/views/garden/AddNewPlantFragment.java
+++ app/src/main/java/com/example/sep4_android/views/garden/AddNewPlantFragment.java
@@ -7,14 +7,35 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.sep4_android.R;
 
 public class AddNewPlantFragment extends Fragment {
+  private EditText name;
+  private EditText landArea;
+  private EditText city;
+  private EditText street;
+  private EditText streetNumber;
+  private Button addGardenBtn;
+  private View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add_new_plant, container, false);
+        view = inflater.inflate(R.layout.fragment_add_garden, container, false);
+        prepareUI();
+    }
+
+    private void prepareUI() {
+        name = view.findViewById(R.id.input_garden_name);
+        landArea =view.findViewById(R.id.input_land_area);
+        city = view.findViewById(R.id.input_city);
+        street = view.findViewById(R.id.input_street);
+        streetNumber = view.findViewById(R.id.input_street_number);
+    }
+    
     }
 }
