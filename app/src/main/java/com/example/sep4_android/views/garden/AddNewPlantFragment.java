f0d0a76347ff0b3701209e6612d8d22be79f49ae
Estera <dumi_estera@yahoo.com>
2021-04-22 14:25:33 +0200

    fixed the image

--- app/src/main/java/com/example/sep4_android/views/garden/AddNewPlantFragment.java
+++ app/src/main/java/com/example/sep4_android/views/garden/AddNewPlantFragment.java
@@ -14,28 +14,29 @@ import android.widget.TextView;
 import com.example.sep4_android.R;
 
 public class AddNewPlantFragment extends Fragment {
-  private EditText name;
-  private EditText landArea;
-  private EditText city;
-  private EditText street;
-  private EditText streetNumber;
-  private Button addGardenBtn;
-  private View view;
+    private EditText name;
+    private EditText landArea;
+    private EditText city;
+    private EditText street;
+    private EditText streetNumber;
+    private Button addGardenBtn;
+    private View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_add_garden, container, false);
         prepareUI();
+        return view;
     }
+
 
     private void prepareUI() {
         name = view.findViewById(R.id.input_garden_name);
-        landArea =view.findViewById(R.id.input_land_area);
+        landArea = view.findViewById(R.id.input_land_area);
         city = view.findViewById(R.id.input_city);
         street = view.findViewById(R.id.input_street);
         streetNumber = view.findViewById(R.id.input_street_number);
     }
-    
-    }
+
 }
